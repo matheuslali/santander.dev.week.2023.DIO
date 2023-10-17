08875c75da19e0b5307db31590f831e0226ebb6e
Matheus Lali <matheus.lali@hotmail.com>
2023-10-16 20:07:31 -0400

    Novo commit inicial

---
+++ src/main/java/me/dio/santander/dev/week3/Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander.dev.week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander/dev/week3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander.dev.week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
